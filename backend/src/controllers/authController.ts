666dc76e4ef1f3fbf172b40fe2b9538fe58351aa
Jachak <jachak.sekhon@hotmail.com>
2025-07-12 20:14:38 -0700

    removed .env from version control

---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,69 @@
+import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { PrismaClient } from '@prisma/client';
+import { createUser, findUserByEmail } from '../services/userService';
+import dotenv from 'dotenv'
+
+const prisma = new PrismaClient();
+dotenv.config();
+
+export const signup = async (req: Request, res: Response) => {
+    const { firstname, lastname, email, password } = req.body;
+
+    try {
+
+        // Check if email already exists
+        const existing = await prisma.user.findUnique({where: { email }});
+
+        if (existing) 
+            return res.status(400).json({ error: 'An account with that email already exists!'});
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await createUser({
+            data: { 
+                firstname, 
+                lastname,
+                email, 
+                passsword: hashedPassword
+            },
+        });
+
+        res.status(201).json({
+            message: 'User created successfully',
+            user: {
+                id: user.id,
+                firstname: user.firstname,
+                lastname: user.lastname,
+                email: user.email,
+                createdAt: user.createdAt
+            },
+        });
+    } catch (err) {
+        console.error('Signup error:', err);
+        res.status(500).json({ error: "Internal server error" });
+    }
+}
+
+export const login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await findUserByEmail(email);
+        if (!user) {
+            return res.status(401).json({ error: "Email not found"});
+        }
+
+        const isValid = await bcrypt.compare(password, user.password);
+        if (!isValid) {
+            return res.status(401).json({ error: "Incorrect password!"});
+        }
+
+        const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: "1d" });
+        res.json({ message: 'Login successful', token});
+
+    } catch (err){
+        res.status(500).json({ error: "Error logging in"})
+    }
+}
--- backend/src/services/userService.ts
+++ backend/src/services/userService.ts
@@ -5,3 +5,11 @@ const prisma = new PrismaClient();
 export const getUsers = async () => {
   return await prisma.user.findMany();
 };
+
+export const createUser = async (data: any) => {
+    return await prisma.user.create({ data });
+}
+
+export const findUserByEmail = async (email: String) => {
+    return await prisma.user.findUnique({ where: { email }})
+}
