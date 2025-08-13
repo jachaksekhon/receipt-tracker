3fcf3178e7e07ef71e6d212148b75c7aa43fd60d
Jachak <jachak.sekhon@hotmail.com>
2025-08-13 12:49:54 -0700

    CCS-27: Created response codes file and removed all ints from code files

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from 'express';
 import UserService from '../services/userService';
-import { ResponseStrings } from '../strings/ResponseStrings';
+import { ResponseStrings } from '../constants/ResponseStrings';
+import { ResponseCodes } from '../constants/ResponseCodes';
 
 export const getAllUsers = async(req: Request, res: Response) => {
     try {
         const users = await UserService.getUsers();
         res.json(users);
     } catch (error) {
-        res.status(500).json({ error: ResponseStrings.GettingUsersError })
+        res.status(ResponseCodes.InternalServerError).json({ error: ResponseStrings.GettingUsersError })
     }
 }
