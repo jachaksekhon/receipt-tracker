af17acc91b5e37ddaee225eea1655d853324ef47
Jachak <jachak.sekhon@hotmail.com>
2025-08-26 21:28:24 -0700

    CCS-22: created the useAuth method to allow for pages to check login status

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,9 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+
+import { AuthProvider } from "./auth/useAuth";
+
 import Home from "./pages/Home";
 import Login from "./pages/Auth/Login";
 import Signup from "./pages/Auth/Signup";
@@ -11,15 +14,17 @@ import ForgotPassword from "./pages/Auth/ForgotPassword";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/forgotpassword" element={<ForgotPassword />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
